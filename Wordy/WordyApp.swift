b593b5813bf40ee0aa571c4d36d263717bb8af76
Vlad Sytnik <64352783+Vladsytnik@users.noreply.github.com>
2022-12-10 14:41:05 +0300

    init project

---
+++ Wordy/WordyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordyApp.swift
+//  Wordy
+//
+//  Created by Vlad Sytnik on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
